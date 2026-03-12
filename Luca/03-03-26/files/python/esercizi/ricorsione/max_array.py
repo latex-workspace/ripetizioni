11b0bfec692ad8e1d8a847ef19123a6a1d3b97f5
Mattia Marini <marmattia26@gmail.com>
2026-03-12 21:31:56 +0100

    Quantità di moto, energia e lavoro

--- Luca/03-03-26/files/python/esercizi/ricorsione/max_array.py
+++
@@ -1,8 +0,0 @@
-def max_rec(v: list[int]) -> int:
-    if len(v) == 1:
-        return v[0]
-    return max(v[0], max_rec(v[1:]))
-
-
-v = [1, 6, 8, 2, 14, 6, 34, -3, 7]
-print(f"max: {max_rec(v)}")
--- Luca/21-02-26/files/fatti_insieme/21-02-26/inv.py
+++
@@ -1,9 +0,0 @@
-def inv(l: list[int]) -> list[int]:
-    if len(l) == 1:
-        return l
-    else:
-        return [l[-1]] + inv(l[:-1])
-
-
-l = [1, 2, 3, 4, 5]
-print(inv(l))
